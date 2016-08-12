8bc7665d4763c4d2c8ad3656124138e695aac208
Philipp Mainz <pmainz@varion.de>
2016-08-12 17:36:22 +0200

    changed masonry css + added get basic function to masonry

--- brainsapp.js
+++ brainsapp.js
@@ -56,6 +56,9 @@
             for (i = 0; i < array.length; i++) {
                 o[i % cols].push(array[i]);
             }
+            setTimeout(function(){
+                window.ba.masonry.get_basic(elem);
+            },500);
             return o;
         };
 
@@ -67,21 +70,36 @@
                 for (i = 0; i < cols; i++) {
                     o.push(array[i]);
                 }
-                array.splice(0,i);
+                array.splice(0, i);
             } else {
                 for (i = 0; i < cols; i++) {
                     o.push([]);
                 }
             }
-            array = [].concat.apply([],array);
+            array = [].concat.apply([], array);
             for (i = 0; i < array.length; i++) {
                 o[i % cols].push(array[i]);
             }
             return o;
         };
+        window.ba.masonry.get_basic = function (elem) {
+            window.ba.masonry.config.temp = {};
+            window.ba.masonry.config.temp.colum = core(elem);
+            var t = [];
+            for (var i = 0; i < window.ba.masonry.config.temp.colum; i++) {
+                t.push($('#' + elem + '>div:nth-child(' + (i + 1) + ')').height());
+            }
+            window.ba.masonry.config.temp.height = t;
+            return window.ba.masonry.config.temp;
+        };
+        window.ba.masonry.get_by_id = function (elem, id) {
 
+        };
+        window.ba.masonry.get_index = function () {
+
+        };
         var core = function (elem) {
-            var w = document.getElementsByClassName(elem)[0].offsetWidth;
+            var w = document.getElementById(elem).offsetWidth;
             var mq = window.ba.var.media_queries;
             var col = window.ba.masonry.config.colum;
             if (w > mq.xl) return col.xl;
