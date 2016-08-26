b17b5c0bb9da63a52cc79d33cc85004d91a09eb2
Philipp Mainz <pmainz@varion.de>
2016-08-26 12:55:22 +0200

    minor changes

--- brainsapp.js
+++ brainsapp.js
@@ -103,26 +103,7 @@
   })();
 
 
-  ///* When the user clicks on the button,
-  //toggle between hiding and showing the dropdown content */
-  //function myFunction() {
-  //    document.getElementById("myDropdown ").classList.toggle("show ");
-  //}
-  //
-  //// Close the dropdown if the user clicks outside of it
-  //window.onclick = function(event) {
-  //  if (!event.target.matches('.dropbtn')) {
-  //
-  //    var dropdowns = document.getElementsByClassName("dropdown-content ");
-  //    var i;
-  //    for (i = 0; i < dropdowns.length; i++) {
-  //      var openDropdown = dropdowns[i];
-  //      if (openDropdown.classList.contains('show')) {
-  //        openDropdown.classList.remove('show');
-  //      }
-  //    }
-  //  }
-  //};
+
   $(function () {
     'use strict';
     $('.ba-dropbtn').on('click', function (e) {
@@ -141,4 +122,5 @@
       }
     }
   };
+
 }());
